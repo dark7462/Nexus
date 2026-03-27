f79fa071990afc47edca68a5044e0b081a7ab567
dark7462 <anu870906@gmail.com>
2026-03-27 09:58:05 +0530

    shiftedToPostgres

--- src/main/java/com/dark/model/User.java
+++ src/main/java/com/dark/model/User.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 
 @Entity
+@jakarta.persistence.Table(name = "users")
 @com.fasterxml.jackson.annotation.JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class User {
 
